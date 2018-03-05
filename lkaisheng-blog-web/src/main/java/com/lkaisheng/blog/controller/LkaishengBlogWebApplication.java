385d51bec94fd95b00cae40a663e8f0a368a871f
likaisheng <likaisheng@chunbo.com>
2018-03-05 17:29:28 +0800

    init blog and add blog-web

---
+++ lkaisheng-blog-web/src/main/java/com/lkaisheng/blog/controller/LkaishengBlogWebApplication.java
@@ -0,0 +1,12 @@
+package com.lkaisheng.blog.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LkaishengBlogWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LkaishengBlogWebApplication.class, args);
+	}
+}

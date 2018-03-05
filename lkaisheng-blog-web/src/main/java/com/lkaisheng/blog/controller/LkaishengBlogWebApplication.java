dcd16ada69642f993343a9030f00e95de5b7c0ab
likaisheng <likaisheng@chunbo.com>
2018-03-05 21:24:20 +0800

    调整spring boot的版本

--- lkaisheng-blog-web/src/main/java/com/lkaisheng/blog/controller/LkaishengBlogWebApplication.java
+++ lkaisheng-blog-web/src/main/java/com/lkaisheng/blog/controller/LkaishengBlogWebApplication.java
@@ -2,9 +2,17 @@ package com.lkaisheng.blog.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @SpringBootApplication
 public class LkaishengBlogWebApplication {
+
+	@RequestMapping("/index")
+	public String index(){
+		return "index";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LkaishengBlogWebApplication.class, args);

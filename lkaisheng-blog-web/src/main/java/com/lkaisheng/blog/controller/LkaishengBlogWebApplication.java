19863c86b68014c67b7ed2994bdda6e33ea5940b
likaisheng001 <1805318928@qq.com>
2018-03-06 22:02:18 +0800

    配置消息转换器

--- lkaisheng-blog-web/src/main/java/com/lkaisheng/blog/controller/LkaishengBlogWebApplication.java
+++ lkaisheng-blog-web/src/main/java/com/lkaisheng/blog/controller/LkaishengBlogWebApplication.java
@@ -2,8 +2,12 @@ package com.lkaisheng.blog.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.nio.charset.Charset;
 
 @Controller
 @SpringBootApplication
@@ -16,5 +20,12 @@ public class LkaishengBlogWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LkaishengBlogWebApplication.class, args);
+	}
+
+	// 自定义消息转换器
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter(){
+		StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+		return converter;
 	}
 }

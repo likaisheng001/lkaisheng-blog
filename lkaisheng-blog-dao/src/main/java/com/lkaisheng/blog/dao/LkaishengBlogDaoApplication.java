5f742578b392eb0a6c3920fad6029faf43ca4919
likaisheng <likaisheng@chunbo.com>
2018-03-06 10:27:14 +0800

    将多余的@SpringBootApplication,@SpringBootTest注解去掉，方便打包

--- lkaisheng-blog-dao/src/main/java/com/lkaisheng/blog/dao/LkaishengBlogDaoApplication.java
+++ lkaisheng-blog-dao/src/main/java/com/lkaisheng/blog/dao/LkaishengBlogDaoApplication.java
@@ -3,7 +3,7 @@ package com.lkaisheng.blog.dao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LkaishengBlogDaoApplication {
 
 	public static void main(String[] args) {
